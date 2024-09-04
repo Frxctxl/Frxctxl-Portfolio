e586b3d736cef7f75a4c0acfe5f3402dd1a8263f
Jay <jayden_benston@yahoo.com>
2024-09-04 00:12:02 -0400

    added link to footer

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -3,7 +3,9 @@ function Footer() {
   return (
     <footer className="grow-0 py-4 flex justify-around shrink-0 bg-Gunmetal text-white">
       <h1 className="self-center">804-718-1041</h1>
-      <img src={logo} alt="" className="size-9 self-center" />
+      <a href="https://www.github.com/Frxctxl">
+        <img src={logo} alt="" className="size-9 self-center" />
+      </a>
     </footer>
   )
 }
